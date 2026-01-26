4cf869e20d415cdb6c34ee25cc4b244b81049fd7
Carlos V. <cevabarcvilla@gmail.com>
2026-01-26 14:55:52 -0300

    Java FX - Chatper 2 - Java FX Properties - Properties listeners, Invalidators Listeners, Change Listeners

--- src/main/java/com/ceva/jfx/ch02/G_MyShapesProperties.java
+++ src/main/java/com/ceva/jfx/ch02/G_MyShapesProperties.java
@@ -4,11 +4,14 @@ import javafx.animation.Animation;
 import javafx.animation.Interpolator;
 import javafx.animation.RotateTransition;
 import javafx.application.Application;
+import javafx.beans.binding.When;
+import javafx.beans.value.ObservableObjectValue;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.effect.Reflection;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.CycleMethod;
 import javafx.scene.paint.LinearGradient;
@@ -19,7 +22,7 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
-public class A_MyShapes extends Application {
+public class G_MyShapesProperties extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         // 1. creacion de nodos Ellipse y Text
@@ -43,45 +46,57 @@ public class A_MyShapes extends Application {
 
         Text text = new Text("My Shapes");
         text.setFont(new Font("Arial Bold", 24));
+
+        Text text2 = new Text("Animation Status: ");
+        text2.setFont(new Font("Arial Bold", 18));
+
             // 1.3 Agregamos un reflejo al texto
             Reflection r = new Reflection();
             r.setFraction(.8);
             r.setTopOffset(1.0);
             text.setEffect(r);
 
-            // 1.4 Configurando Acciones: creamos una manejador de evento click (event handler) y lo agregamos al nodo text
-            text.setOnMouseClicked(mouseEvent -> {
-                System.out.println(mouseEvent.getSource().getClass() + " hizo click");
-            });
+        StackPane stackPane = new StackPane();
+        stackPane.getChildren().addAll(ellipse, text);
+        VBox vBox = new VBox(stackPane, text2);
+        vBox.setAlignment(Pos.CENTER);
+        vBox.setSpacing(50.0);
 
-        // 2. container StackPane
-        StackPane container = new StackPane();
-            // 2.1 Aplicamos Animacion: RotateTransition de 2.5 segundos
-            RotateTransition rotate = new RotateTransition(Duration.millis(2500), container);
-            rotate.setToAngle(360); // la animacion continua linealmente hasta el angulo 360
-            rotate.setFromAngle(0); // la animacion inicia en el angulo 0
-            rotate.setInterpolator(Interpolator.LINEAR);
-            // adjuntamos un manejador de evento click. La animacion se dispara cuando el usuario hace click dentro del container
-            container.setOnMouseClicked(mouseEvent -> {
-                // la animacion se aplica a todos los elementos secundarios del StackPane
-                // comprobamos el estado de la transicion.
-                if(rotate.getStatus().equals(Animation.Status.RUNNING)){
-                    // si la animacion esta en ejecucion, la pausamos
-                    rotate.pause();
-                }
-                else{
-                    rotate.play();
-                }
-            });
+        // Define RotateTransition for stackPane
+        RotateTransition rotate = new RotateTransition(Duration.millis(2500), stackPane);
+        rotate.setToAngle(360);
+        rotate.setFromAngle(0);
+        rotate.setInterpolator(Interpolator.LINEAR);
 
-        container.getChildren().addAll(ellipse, text);
-//        container.setAlignment(text, Pos.BOTTOM_CENTER); // Alineamos unicamente el nodo text
+        // Invalidation Listener using lambda expression
+        rotate.statusProperty().addListener(observable -> {
+            text2.setText("Animation status: " +
+                    ((ObservableObjectValue<Animation.Status>)observable).getValue());
+            text2.setText("Animation status: " + rotate.getStatus());
+        });
 
-        // 3. Add contendor a la escena
-        Scene scene = new Scene(container, 350, 230,Color.LIGHTYELLOW);
+        // Change Listener using lambda expression
+        rotate.statusProperty().addListener((observableValue, oldValue, newValue) -> {
+            text2.setText("Was " + oldValue + ", Now " + newValue);
+        });
 
-        // 4. Add Escena al Escenario
-        stage.setTitle("My Shapes with JavaFX");
+        // Bind expression with When
+        text2.strokeProperty().bind(new When(rotate.statusProperty()
+                .isEqualTo(Animation.Status.RUNNING)) .then(Color.GREEN).otherwise(Color.RED));
+
+        // configure mouse click handler
+        stackPane.setOnMouseClicked(mouseEvent -> {
+            if (rotate.getStatus().equals(Animation.Status.RUNNING)) {
+                rotate.pause();
+            } else {
+                rotate.play();
+            }
+        });
+
+        Scene scene = new Scene(vBox, 350, 350, Color.LIGHTYELLOW);
+        //scene.getStylesheets().add("/styles/Styles.css");
+
+        stage.setTitle("MyShapesProperties");
         stage.setScene(scene);
         stage.show();
     }
